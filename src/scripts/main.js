d3b8e4a1c613034bbe4d995ec1215e78a94d2b25
Valentyn <valpreacher@gmail.com>
2017-03-13 18:32:28 +0200

    lesson results 13-03-2017

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,46 @@
-import 'jquery';
+import $ from 'jquery';
+import _ from 'lodash';
 import 'bootstrap-sass';
+import { getProductsForCategory } from './products';
+
+const productTemplate = _.template(`
+<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 bw-grid-product">
+    <img class="bw-grid-product-image"
+         src="http://placehold.it/500x500" />
+    <span class="bw-grid-product-title">
+        <%= title %>
+    </span>
+    <span class="bw-grid-product-price">
+        $<%= price %>
+    </span>
+</div>
+`);
 
 $(window.document).ready(() => {
-  console.log('hello world');
+  const $tabHandles = $('a[data-toggle="tab"]');
+
+  $tabHandles.on('show.bs.tab', (event) => {
+    const $tabHandle = $(event.target);
+    const category = $tabHandle.data('category');
+    const $tabContent = $('#' + category + ' .row');
+
+    getProductsForCategory(category)
+      .then((products) => {
+        let productsHtml = '';
+
+        products.forEach((product) => {
+          productsHtml += productTemplate(product);
+        });
+
+        $tabContent.html(productsHtml);
+      });
+  });
+
+  $tabHandles.on('hide.bs.tab', (event) => {
+    const $tabHandle = $(event.target);
+    const category = $tabHandle.data('category');
+    const $tabContent = $('#' + category + ' .row');
+
+    $tabContent.html('');
+  });
 });
